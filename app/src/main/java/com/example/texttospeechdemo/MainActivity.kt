2cc8606d3a2c3e1906c9206b742dae5d5cf2209b
Lee-IDLE <nidus0526@gmail.com>
2023-12-27 23:56:54 +0900

    안 올린거 있어서 추가함

--- app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
+++ app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.texttospeechdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import com.example.texttospeechdemo.databinding.ActivityMainBinding
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private lateinit var binding :ActivityMainBinding
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tts = TextToSpeech(this@MainActivity, this)
+
+        binding.btnSpeak.setOnClickListener{view ->
+            if(binding?.etEnteredText?.text!!.isEmpty()){
+                Toast.makeText(this@MainActivity, "Enter a text to speak.",
+                    Toast.LENGTH_LONG).show()
+            }else{
+                speakOut(binding.etEnteredText?.text!!.toString())
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(tts != null){
+            tts?.stop()
+            tts?.shutdown()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.KOREAN)
+
+            if(result == TextToSpeech.LANG_MISSING_DATA ||
+                result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Toast.makeText(this@MainActivity, "Woops... Sorry! ERROR!",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun speakOut(text: String){
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
